21bc5ffa1e57124ec803c0352cfe165388d328b0
Agustin Burgos <89648043+aeburgos00@users.noreply.github.com>
2026-07-12 15:44:23 -0300

    Refactor liquidaciones card data and icons

--- frontend/routeLog/src/components/datos/dataKPILiquidaciones.jsx
+++ frontend/routeLog/src/components/datos/dataKPILiquidaciones.jsx
@@ -1,18 +1,13 @@
-import TotalEnviosIcon from '@mui/icons-material/Inventory';
-import ValorTotalIcon from '@mui/icons-material/MonetizationOnOutlined';
-import EnviosLiquidadosIcon from "@mui/icons-material/LocalShipping";
-import ValorLiquidadoIcon from '@mui/icons-material/PriceCheckOutlined';
-import PorcentajeIcon from '@mui/icons-material/Percent';
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-import PriceCheckIcon from '@mui/icons-material/PriceCheck';
-import PagoPendienteIcon from '@mui/icons-material/AccountBalanceOutlined';
+import TotalLiqIcon from '@mui/icons-material/ReceiptLong';
+import LiqCerradasIcon from '@mui/icons-material/Task';
+import LiqAbiertasIcon from '@mui/icons-material/RequestPage';
+import TransporistasIcon from "@mui/icons-material/LocalShipping";
 
 export const cardLiquidaciones = [
-{id:"cantidad_liquidaciones", icono: ReceiptLongIcon, titulo: "Total liquidaciones", color:"#3b82f6"},
-{id:"valor_total", icono: ValorTotalIcon, titulo: "Valor total de liquidaciones", color:"#f59e0b"},
-{id:"pago_realizado", icono: PriceCheckIcon, titulo: "Pago realizado", color:"#65a30d"},
-{id:"pago_pendiente", icono: PagoPendienteIcon, titulo: "Pago pendiente", color:"#718096"},
-{id:"pct_paquetes_liquidados", icono: PorcentajeIcon, titulo: "% Liquidaciones cerradas", color:"#713dfe"},
+{id:"total_liquidado", icono: TotalLiqIcon, titulo: "Total liquidado", color:"#3b82f6"},
+{id:"liq_cerradas", icono: LiqCerradasIcon, titulo: "Liquidaciones cerradas", color:"#65a30d"},
+{id:"liq_abiertas", icono: LiqAbiertasIcon, titulo: "Liquidaciones abiertas", color:"#ef4444"},
+{id:"transportistas_en_periodo", icono: TransporistasIcon, titulo: "Transportistas en periodo", color:"#713dfe", }
 ];
 
 export default cardLiquidaciones;
